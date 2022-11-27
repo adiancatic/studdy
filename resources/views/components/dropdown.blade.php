702cae353e0fdbea7aac48f6d5b82be7373b46f2
adiancatic <adiancatic@gmail.com>
2022-11-27 18:28:06 +0100

    Dropdown component

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,8 @@
+<div class="dropdown">
+    <button class="dropdown-btn" type="button" data-bs-toggle="dropdown">
+        {{ $toggle }}
+    </button>
+    <div class="dropdown-menu">
+        {{ $slot }}
+    </div>
+</div>
--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,38 +1,48 @@
 <nav class="nav-sidebar">
     <div class="nav-sidebar__header">
-
         <a class="nav-sidebar__logo" href="{{ url('/') }}">
             <img class="logo"
                  src="{{ asset('assets/logo.png') }}"
                  srcset="{{ asset('assets/logo.png') }} 1x, {{ asset('assets/logo@2x.png') }} 2x"
                  alt="{{ config('app.name') }}">
         </a>
-
         <div class="nav-sidebar__user">
-            {{ Auth::user()->name }}
-            <div class="user-dropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
 
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
-            </div>
-        </div>
+            <x-dropdown>
+                <x-slot:toggle>
+                    <x-user-avatar/>
+                </x-slot>
 
-        @if(\App\View\Components\Navigation::COMPOSERS)
-            <ul class="nav-sidebar__modules">
-                @foreach(\App\View\Components\Navigation::getComposers() as $composer)
-                    <li class="nav-sidebar__module-item">
-                        <a href="{{ \App\View\Components\Navigation::TEMP_URL_MAP[$composer::class] }}" class="nav-sidebar__module-item-anchor">
-                            <i class="nav-sidebar__module-item-icon fa-fw fas fa-{{ $composer->getIcon() }}"></i>
-                            <span class="nav-sidebar__module-item-title">{{ $composer->getTitle() }}</span>
-                        </a>
+                <ul class="dropdown-section">
+                    <li class="dropdown-item dropdown-info">
+                        <div class="user-name">{{ Auth::user()->name }}</div>
+                        <div class="user-email">{{ Auth::user()->email }}</div>
                     </li>
-                @endforeach
-            </ul>
-        @endif
+                </ul>
+                <ul class="dropdown-section">
+                    <li class="dropdown-item dropdown-action">
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            <i class="fas fa-door-open"></i>
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
+                    </li>
+                </ul>
+            </x-dropdown>
 
+        </div>
     </div>
+
+    @if(\App\View\Components\Navigation::COMPOSERS)
+        <ul class="nav-sidebar__modules">
+            @foreach(\App\View\Components\Navigation::getComposers() as $composer)
+                <li class="nav-sidebar__module-item">
+                    <a href="{{ \App\View\Components\Navigation::TEMP_URL_MAP[$composer::class] }}" class="nav-sidebar__module-item-anchor">
+                        <i class="nav-sidebar__module-item-icon fa-fw fas fa-{{ $composer->getIcon() }}"></i>
+                        <span class="nav-sidebar__module-item-title">{{ $composer->getTitle() }}</span>
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    @endif
 </nav>
