153db1b93749d15700e0a1e87c873be3b3ac856e
adiancatic <adiancatic@gmail.com>
2022-12-02 19:30:23 +0100

    Removed Welcome view and fixed redirect for '/'

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return \Illuminate\Support\Facades\Auth::guest()
+        ? redirect("/login")
+        : redirect(\App\Providers\RouteServiceProvider::DASHBOARD);
 });
 
 Auth::routes();
