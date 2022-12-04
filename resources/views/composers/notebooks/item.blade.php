7a3d51d55025b02437fd50f40561055c6a3ff610
adiancatic <adiancatic@gmail.com>
2022-12-04 19:34:04 +0100

    Single Notebook's list view improvements

--- resources/views/composers/notebooks/item.blade.php
+++ resources/views/composers/notebooks/item.blade.php
@@ -1,18 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="p-4">
-        <h1>{{ $notebook->title }}</h1>
-        @if(isset($notebook->notes))
-            <ul>
-                @foreach($notebook->notes as $note)
-                    <li>
-                        <a href="/notebooks/{{ $notebook->id }}/{{ $note->id }}">
-                            <span>{{ $note->title }}</span>
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
-        @endif
+
+    <div>
+        <div class="item-list">
+            <div class="item-list__header">
+                <h1 class="item-list__title">
+                    <i class="far fa-note"></i>
+                    {{ __("Notes") }}
+                </h1>
+            </div>
+
+            @if(! $notebook->notes->isEmpty())
+                <div class="item-list__body">
+                    @foreach($notebook->notes as $note)
+                        <div class="item-list__item">
+                            <span class="item-list__item-index">{{ $loop->iteration }}</span>
+                            <span class="item-list__item-title">
+                                {{ $note->title }}
+                                <a class="btn btn-default btn-xs item-list__item-cta" href="/notebooks/{{ $notebook->id }}/{{ $note->id }}">{{ __("Open") }}</a>
+                            </span>
+                            <span class="item-list__item-date">
+                                <i class="far fa-calendar"></i>
+                                {{ $note->created_at->format("d M Y") }}
+                            </span>
+                            <span class="item-list__item-author">{{ $note->author }}</span>
+                        </div>
+                    @endforeach
+                </div>
+            @else
+                {{-- todo add empty section --}}
+                There are no notes
+            @endif
+        </div>
+
     </div>
 @endsection
