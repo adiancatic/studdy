37356d53d9ab4a96780cf7e341b02517030ababa
adiancatic <adiancatic@gmail.com>
2022-12-11 01:43:39 +0100

    Moved JS file back to top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     @vite([
         'resources/plugins/fontawesome/css/all.min.css',
         'resources/scss/app.scss',
+        'resources/js/app.js',
     ])
 @endsection
 
@@ -17,8 +18,5 @@
         </main>
 
         @livewireScripts
-        @vite([
-            'resources/js/app.js',
-        ])
     </body>
 @endsection
