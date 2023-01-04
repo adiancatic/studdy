cd0e81ee3072a88c18f5f660cdca219268397e7d
adiancatic <adiancatic@gmail.com>
2023-01-04 16:53:31 +0100

    Unified way to save data through data binding

---
+++ app/Http/Livewire/ModelComponent.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Helpers\DI;
+use Illuminate\Database\Eloquent\Model;
+use Livewire\Component;
+
+class ModelComponent extends Component
+{
+    /** @var Model */
+    public $model;
+
+    protected $listeners = [
+        "refresh" => '$refresh',
+        "emittedData",
+    ];
+
+    public function mount($params = null)
+    {
+        if (! $this->model) {
+            $this->model = DI::get(static::MODEL);
+        }
+
+        if ($params) {
+            $this->model = $this->model::where(
+                array_map(fn ($key, $val) => [$key, $val], array_keys($params), array_values($params))
+            )->first();
+        }
+    }
+
+    public function validateAndSave()
+    {
+        $this->validate();
+        $this->save();
+    }
+
+    public function save()
+    {
+        $this->model->save();
+        $this->emitUp("refresh");
+    }
+
+    public function emittedData($data)
+    {
+        $this->model->fill($data);
+    }
+
+    public function delete()
+    {
+        $this->model->delete();
+        $this->emitUp("refresh");
+    }
+}
