9111f3ccb0a43d0d048e71c250960f0a277fbac5
adiancatic <adiancatic@gmail.com>
2023-01-06 21:47:49 +0100

    Added notebooks relation method to User class

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -43,9 +42,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function subjects(): HasMany
+    public function subjects(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(Subject::class);
+    }
+
+    public function notebooks(): \Illuminate\Database\Eloquent\Relations\HasManyThrough
+    {
+        return $this->hasManyThrough(Notebook::class, Subject::class);
     }
 
     public function events(): \Illuminate\Database\Eloquent\Relations\HasMany
