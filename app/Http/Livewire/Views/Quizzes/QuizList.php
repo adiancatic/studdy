504142d4187dea97e06b9124abaf33a9ca48e960
adiancatic <adiancatic@gmail.com>
2023-01-18 11:57:03 +0100

    Quiz list view

--- app/Http/Livewire/Views/Quizzes/QuizList.php
+++ app/Http/Livewire/Views/Quizzes/QuizList.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Views\Quizzes;
 
 use App\Helpers\BreadcrumbTrait;
 use App\Http\Livewire\Views\AbstractView;
+use Illuminate\Support\Facades\Auth;
 
 class QuizList extends AbstractView
 {
@@ -11,6 +12,26 @@ class QuizList extends AbstractView
 
     protected const TITLE = "Quizzes";
     public const ICON = "question-circle";
+
+    protected $listeners = [
+        "refresh" => '$refresh',
+    ];
+
+    public $quizzes;
+
+    public function updateOrder($items)
+    {
+        $order = collect($items)->pluck("order", "value")->toArray();
+
+        foreach ($this->quizzes as $quiz) {
+            // dump("before: {$quiz->order} | after: {$order[$quiz->id]}");
+            $quiz->order = $order[$quiz->id];
+            $quiz->save();
+        }
+
+        $this->quizzes = Auth::user()->fresh()->quizzes()->orderBy("order")->get();
+        $this->emit("refresh");
+    }
 
     public function breadcrumbConf()
     {
@@ -22,6 +43,8 @@ class QuizList extends AbstractView
 
     public function render()
     {
+        $this->quizzes = Auth::user()->quizzes()->orderBy("order")->get();
+
         return view('livewire.views.quizzes.quiz-list');
     }
 
--- resources/views/livewire/views/quizzes/quiz-list.blade.php
+++ resources/views/livewire/views/quizzes/quiz-list.blade.php
@@ -1,3 +1,60 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    @livewire("components.breadcrumbs", ["node" => static::class])
+
+    <div class="item-list">
+        @if(! $quizzes->isEmpty())
+            <div class="item-list__header">
+                <h1 class="item-list__title">
+                    <i class="far fa-question-circle"></i>
+                    {{ __("Quizzes") }}
+                </h1>
+                <div class="item-list__actions">
+                    <button class="btn btn-blank" onclick="openModal('views.quizzes.modal.edit-quiz-modal')"><i class="fal fa-plus"></i></button>
+                </div>
+            </div>
+
+            <div wire:sortable="updateOrder" class="item-list__body">
+                @foreach($quizzes as $quiz)
+                    @ob
+                        <x-dropdown>
+                            <x-slot:toggle>
+                                <i class="far fa-fw fa-ellipsis-v"></i>
+                            </x-slot:toggle>
+
+                            <x-dropdown.item type="action">
+                                <button type="button" wire:click="edit('views.quizzes.modal.edit-quiz-modal')">
+                                    <i class="far fa-fw fa-pen"></i>{{ __("Edit") }}
+                                </button>
+                            </x-dropdown.item>
+                            <x-dropdown.item type="action">
+                                <button type="button" wire:click="confirmAndDelete">
+                                    <i class="far fa-fw fa-trash"></i>{{ __("Delete") }}
+                                </button>
+                            </x-dropdown.item>
+                        </x-dropdown>
+                    @endob(dropdown)
+
+                    <livewire:components.list-item :index="$loop->iteration" :model="$quiz" :isSortable="true" :dropdown="$dropdown" wire:key="{{ $quiz->id }}" />
+                @endforeach
+
+                <div class="item-list__item">
+                    <span class="item-list__item-index"></span>
+                    <button type="button" class="btn btn-default" onclick="openModal('views.quizzes.modal.edit-quiz-modal')">{{ __("Add quiz") }}</button>
+                </div>
+            </div>
+        @else
+            @ob
+                <button type="button" class="btn btn-default" onclick="openModal('views.quizzes.modal.edit-quiz-modal')">
+                    <i class="fal fa-plus"></i>{{ __("Add quiz") }}
+                </button>
+            @endob(cta)
+
+            <x-empty-state.basic
+                icon="question-circle"
+                title="{{ __('No quizzes') }}"
+                description="{{ __('There are no quizzes, add a new one to start') }}"
+                :ctas="[$cta]"
+            />
+        @endif
+    </div>
 </div>
